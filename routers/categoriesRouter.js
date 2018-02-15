a9a4fe655a9a5d4e7ef48b7882a7a161af742524
NikolayTsvetin <nikolaytsvetin@gmail.com>
2018-02-15 23:37:01 +0200

    removed unnecesary stuff

--- routers/categoriesRouter.js
+++ routers/categoriesRouter.js
@@ -1,12 +1,12 @@
-/*var express = require('express'),
+var express = require('express'),
   _ = require('lodash');
 
 require('../polyfills/array');
 
-module.exports = function(db) {
+module.exports = function (db) {
   var router = express.Router();
 
-  router.get('/', function(req, res) {
+  router.get('/', function (req, res) {
     var user = req.user;
     if (!user) {
       res.status(401)
@@ -15,16 +15,16 @@ module.exports = function(db) {
     }
 
     var categories = db('users')
-      .map(function(user) {
+      .map(function (user) {
         var todoCategories = [];
         if (user.todos) {
-          todoCategories = user.todos.map(function(todo) {
+          todoCategories = user.todos.map(function (todo) {
             return todo.category;
           });
         }
         var eventCategories = [];
         if (user.events) {
-          eventCategories = user.events.map(function(event) {
+          eventCategories = user.events.map(function (event) {
             return event.category;
           });
         }
@@ -33,7 +33,7 @@ module.exports = function(db) {
 
     categories = _.chain(categories)
       .flatten(categories, true)
-      .sortBy(function(cat) {
+      .sortBy(function (cat) {
         return cat.toLowerCase();
       }).uniq()
       .value();
@@ -43,52 +43,4 @@ module.exports = function(db) {
     });
   });
   return router;
-};
-*/
-
-var express = require('express'),
-_ = require('lodash');
-
-require('../polyfills/array');
-
-module.exports = function(db) {
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  var user = req.user;
-  if (!user) {
-    res.status(401)
-      .json('Not authorized User');
-    return;
-  }
-
-  var categories = db('users')
-    .map(function(user) {
-      var todoCategories = [];
-      if (user.todos) {
-        todoCategories = user.todos.map(function(todo) {
-          return todo.category;
-        });
-      }
-      var eventCategories = [];
-      if (user.events) {
-        eventCategories = user.events.map(function(event) {
-          return event.category;
-        });
-      }
-      return todoCategories.concat(eventCategories);
-    });
-
-  categories = _.chain(categories)
-    .flatten(categories, true)
-    .sortBy(function(cat) {
-      return cat.toLowerCase();
-    }).uniq()
-    .value();
-
-  res.json({
-    result: categories
-  });
-});
-return router;
 };
--- utils/authorized-user.js
+++ utils/authorized-user.js
@@ -1,28 +1,14 @@
-/*const AUTH_KEY_HEADER_NAME = 'x-auth-key';
-
-module.exports = function(app, db) {
-  app.use(function(req, res, next) {
-    var authKey = req.headers[AUTH_KEY_HEADER_NAME];
-    var user = db('users').find({
-      authKey: authKey
-    });
-    req.user = user || null;
-    next();
-  });
-};
-*/ // This is the old one
-
 const AUTH_KEY_HEADER_NAME = 'x-auth-key';
 
-module.exports = function(app, db){
-    app.use(function(req, res, next) {
-        const authKey = req.headers[AUTH_KEY_HEADER_NAME];
-        const collection = db.get().collection('users');
-        collection.find({}).toArray(function (err, users) {
-            req.user = users.find(a => a.authKey === authKey) || null;
-            // console.log(req.user);
-            next()
-        })
-        
+module.exports = function (app, db) {
+  app.use(function (req, res, next) {
+    const authKey = req.headers[AUTH_KEY_HEADER_NAME];
+    const collection = db.get().collection('users');
+    collection.find({}).toArray(function (err, users) {
+      req.user = users.find(a => a.authKey === authKey) || null;
+      // console.log(req.user);
+      next()
     })
+
+  })
 }
